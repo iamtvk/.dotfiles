783f6bb8a4082afb512471ac2d906275c43f8ef9
Vamshi <vamshikrishnachary123@gmail.com>
2022-08-27 20:30:27 +0530

    dmenu config

---
+++ dmenu/.config/dmenu-distrotube/colors/nord.h
@@ -0,0 +1,9 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#D8DEE9", "#2E3440" },
+	[SchemeSel] = { "#2E3440", "#B8D6E8" },/*cyan #8be9fd*/
+	[SchemeSelHighlight] = { "#A3BE8C", "#2E3440" },
+	[SchemeNormHighlight] = { "#A3BE8C", "#2E3440" },
+	[SchemeOut] = { "#2E3440", "#88C0D0" },
+	[SchemeMid] = { "#D8DEE9", "#2E3440" },
+};
